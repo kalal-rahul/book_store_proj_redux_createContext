3e1881912090ff476cbaa8453b4bc5e85ad799b0
kalal-rahul <aero.rahul05@gmail.com>
2023-03-28 20:04:31 +0530

    handle enter key press

--- src/Components/BookCard.js
+++ src/Components/BookCard.js
@@ -1,6 +1,5 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { DataContext } from "../App";
-import bookImage from '../assests/book-image.png';
 
 export const BookCard = (props) => {
 
@@ -8,6 +7,10 @@ export const BookCard = (props) => {
     const {cart} = useContext(DataContext);
     const {setCart} = useContext(DataContext);
 
+    useEffect(() => {
+        // console.log("Use effect trigerred");
+        setAddToCart("Add to cart");
+    }, [props.availableBooks]); //Re-render the component whenever there is change in state of variable in the array
 
     const handleAddToCart = (eachBookData) => {
 
@@ -44,12 +47,12 @@ export const BookCard = (props) => {
     return (
         <div className='book-card flex-column' key={props.index}>
             <div className='book-image'>
-                <img src={props.eachBookData.bookImage} alt="Image of book" />
+                <img src={props.eachBookData.bookImage} alt="Cover page of book" />
             </div>
             <div className='book-detail'>
                 <h3>{props.eachBookData.bookName}</h3>
                 <p>Rs.{props.eachBookData.cost} /-</p>
-                <button onClick={() => { handleAddToCart(props.eachBookData) }}>{addToCart}</button>
+                <button onClick={() => { handleAddToCart(props.eachBookData) }} >{addToCart}</button>
             </div>
         </div>
     );
--- src/Pages/BookSearch.js
+++ src/Pages/BookSearch.js
@@ -1,4 +1,4 @@
-import  Axios  from "axios";
+import Axios from "axios";
 import { useState } from "react";
 import { BookCard } from "../Components/BookCard";
 import { Navbar } from "../Components/Navbar";
@@ -12,37 +12,59 @@ export const BookSearch = (props) => {
     const handleClick = async () => {
 
         await Axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchedBook}&filter=ebooks&key=AIzaSyDnjxV0J2xUBLBAejecvQwUrxKZSDWWbfE`)
-        .then((response) => {
-            // console.log(response.data);
-            setAvailableBooks(response.data.items)
+            .then((response) => {
+                // console.log(response.data);
+                setAvailableBooks(response.data.items);
 
-        }).catch((error) => {
-            console.log(error);
-            alert("Sorry...\nUnable to fetch data from SERVER");
-        });
+            }).catch((error) => {
+                console.log(error);
+                alert("Sorry...\nUnable to fetch data from SERVER");
+            });
 
 
     };
 
+    const handlePressEnter = async (e) => {
+        if (e.key === "Enter"){
+            await Axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchedBook}&filter=ebooks&key=AIzaSyDnjxV0J2xUBLBAejecvQwUrxKZSDWWbfE`)
+            .then((response) => {
+                // console.log(response.data);
+                setAvailableBooks(response.data.items);
+
+            }).catch((error) => {
+                console.log(error);
+                alert("Sorry...\nUnable to fetch data from SERVER");
+            });
+        }    
+    }
+
+    // const testKeyPress = (e) => {
+    //     if (e.key === "Enter")
+    //         console.log('You must have pressed Enter ');
+    //     else
+    //         console.log("You pressed " + e.key);
+    // };
+
 
     return (
         <div>
-            <Navbar/>
+            <Navbar />
 
             <h1 className='center-text'>Search for Books...</h1>
             <div className="search-container flex">
-                <input type="text" placeholder='Enter the book title...'  onChange={(e) => searchedBook = e.target.value}/>
+                <input type="text" placeholder='Enter the book title...' onChange={(e) => searchedBook = e.target.value}
+                    onKeyDown={handlePressEnter} />
                 <button onClick={handleClick}>Search Book</button>
             </div>
             <div className='book-outer-container flex'>
-            {
+                {
                     availableBooks.map((resultItem, index) => {
-                       
+
 
                         let eachBookData = {
                             bookName: resultItem.volumeInfo.title,
                             cost: resultItem.saleInfo.listPrice?.amount,
-                            bookImage : resultItem.volumeInfo.imageLinks.smallThumbnail
+                            bookImage: resultItem.volumeInfo.imageLinks.smallThumbnail
                         };
 
                         return (
@@ -50,6 +72,7 @@ export const BookSearch = (props) => {
                                 <BookCard
                                     eachBookData={eachBookData}
                                     index={index}
+                                    availableBooks={availableBooks}
                                 />
                             </div>
                         );
